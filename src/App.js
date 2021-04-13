71c6b5349601d5997b7b24cb06bd312d2b426403
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-04-12 22:05:43 -0400

    Adding less

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.less';
 
 class App extends Component {
   render() {
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
     ],
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
